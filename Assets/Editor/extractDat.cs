a22189ff4cad3303cb95fbd170b7aca2f867b439
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2023-04-13 09:39:18 -0300

    assets

---
+++ Assets/Editor/extractDat.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class extractDat : EditorWindow
+{
+    static string path = "";
+    static bool Model;
+    static bool Animation;
+    static string type;
+    static string length;
+    static int startOFFSET;
+    static int endOFFSET;
+    static string savePATH;
+    static int count = 0;
+    //static enum FileType {branch_Compiled_Model,MD_ModelFile };
+    [MenuItem("God Hand/.DAT specs")]
+
+    static void Init()
+    {
+        path = EditorUtility.OpenFilePanel("","","");
+        Model = false;
+        Animation = false;
+
+
+        GetWindow(typeof(extractDat));
+    }
+    public void OnGUI()
+    {
+        if (GUILayout.Button("Load .DAT"))
+        {
+            Init();
+        }
+        EditorGUILayout.LabelField("status:" + Path.GetFileNameWithoutExtension(path));
+
+
+
+
+
+        if (path != null)
+        {
+            EditorGUILayout.LabelField("path: " + path);
+            byte[] sample = File.ReadAllBytes(path);
+            //scr
+            for (int i = 0; i < sample.Length; i++)
+            {
+                if (sample[i] == 115 && sample[i + 1] == 99 && sample[i + 2] == 114 && sample[i + 3] == 0)
+                {
+                    Model = true;
+                    //Debug.Log(i);
+                }
+                //mtb3Z
+                if (sample[i] == 109 && sample[i + 1] == 116 && sample[i + 2] == 98 && sample[i + 3] == 51 && sample[i + 4] == 90)
+                {
+                    Animation = true;
+                }
+            }
+
+            //SCR start offset is at 0x08
+            //SCR end length is at 0x0C
+
+            EditorGUILayout.LabelField("isModel: " + Model);
+            EditorGUILayout.LabelField("isAnimation: " + Animation);
+
+
+            List<byte> startOffset = new List<byte>();
+            List<byte> finishLength = new List<byte>();
+            #region type
+            if (Model)
+            {
+                type = "MD Model";
+                if (Animation)
+                {
+                    type = "Packed DAT file";
+                }
+            }
+            else if (!Model && !Animation)
+            {
+                type = "could not be identified!";
+            }
+            EditorGUILayout.LabelField("type: " + type);
+            #endregion
+            #region findOffset
+            for (int i = 0; i < 4; i++)
+            {
+                byte a = sample[8 + i];
+                int g = System.Convert.ToByte(sample[8 + i]);
+                startOffset.Add(sample[8 + i]);
+            }
+            length = System.BitConverter.ToString(startOffset.ToArray());
+            EditorGUILayout.LabelField("start offset: " + length);
+            #endregion
+            #region findLength
+            for (int i = 0; i < 4; i++)
+            {
+                byte a = sample[12 + i];
+                int g = System.Convert.ToByte(sample[12 + i]);
+                finishLength.Add(sample[12 + i]);
+            }
+            length = System.BitConverter.ToString(finishLength.ToArray());
+            EditorGUILayout.LabelField("length offset: " + length);
+            #endregion
+
+
+            if (GUILayout.Button("Extract MD Model"))
+            {
+                Extract(startOffset.ToArray(), finishLength.ToArray());
+            }
+        }
+    }
+    public void Extract(byte[] start, byte[] end)
+    {
+        string newFile = Path.GetFileNameWithoutExtension(path);
+        savePATH = EditorUtility.SaveFilePanel("", "", newFile, "md");
+
+        byte[] file = File.ReadAllBytes(path);
+        //Debug.Log(file.Length);
+
+        int startPos = System.BitConverter.ToInt32(start, 0);
+        int endPos = System.BitConverter.ToInt32(end, 0);
+        //Debug.Log(startPos + "    " + endPos);
+
+        List<byte> writeFinal = new List<byte>();
+        for (int i = startPos; i < endPos; i++)
+        {
+            writeFinal.Add(file[i]);
+        }
+        File.Create(savePATH).Dispose();
+        File.WriteAllBytes(savePATH, writeFinal.ToArray()); 
+        //Write(writeFinal.ToArray(),0,writeFinal.Count);
+        
+
+
+        //for (int i = 0; i < file.Length;i++)
+        //{
+            //bool reachFinish = false;
+        //    int start = System.BitConverter.ToInt32(startOffset.ToArray(),0);
+        //    Debug.Log(start);
+        //}
+    }
+
+    }
---
+++ Assets/Editor/extractMD.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+//ALWAYS AS 8 GROUPS OF BITS
+
+public class extractMD : EditorWindow
+{
+
+
+    static string path = "";
+
+
+    [MenuItem("God Hand/.MD specs")]
+
+    static void Init()
+    {
+        path = EditorUtility.OpenFilePanel("", "", "md");
+
+        GetWindow(typeof(extractMD));
+    }
+    public void OnGUI()
+    {
+        //EditorGUILayout.TextArea("status: " + Path.GetFileNameWithoutExtension(path));4
+        if (GUILayout.Button("Load .MD"))
+        {
+            Init();
+        }
+        EditorGUILayout.LabelField("status: " + Path.GetFileNameWithoutExtension(path));
+
+        if (path != null)
+        {
+            EditorGUILayout.LabelField("path: " + path);
+            byte[] file = File.ReadAllBytes(path);
+            int boneCount = getBoneCount(file);
+            EditorGUILayout.TextArea("boneCount: " + boneCount);
+
+
+            for (int i = 0; i < boneCount; i += 1)
+            {
+                EditorGUILayout.TextArea("boneAdress: " + getBoneAdress(file, i * 4));
+                //EditorGUILayout.TextArea("boneAdress: " + getBoneAdress(file, i));
+            }
+        }
+
+        //for (int i = 0; i < getBoneCount(file);i++)
+        //{
+        //    EditorGUILayout.TextArea("boneName: " + getBoneName(file,i));
+        //}
+
+
+
+        //string boneList = 
+    }
+
+    string getBoneName(byte[] file, int index)
+    {
+        List<string> name = new List<string>(); //to use index for research, 1 = z_11 & etc...
+        return null;
+    }
+    #region boneCount
+    int getBoneCount(byte[] file)
+    {
+        List<byte> boneOffset = new List<byte>();
+        for (int i = 0; i < 4; i++){
+            boneOffset.Add(file[8 + i]);
+        }
+        int startPos = System.BitConverter.ToInt32(boneOffset.ToArray(), 0);
+       return startPos;
+    }
+    #endregion
+    #region getBoneAdress
+    //0x10
+    //>>bonecount
+    string getBoneAdress(byte[] file,int Additive)
+    {
+        byte[] boneOffset = goToAdress(16 + Additive,4,file);
+        string boneCount = "";
+        for (var i = 0; i < boneOffset.Length; i++)
+        {
+            boneCount = boneCount + " " + boneOffset[i];
+            Debug.Log(i);
+        }
+        return boneCount;
+    }
+    #endregion
+
+    byte[] goToAdress(int start, int ByteAfter, byte[] file)
+    {
+        List<byte> boneOffset = new List<byte>();
+        for (int i = 0; i < ByteAfter; i++)
+        {
+            boneOffset.Add(file[start + i]);
+            //Debug.Log(boneOffset[i]);
+        }
+        return boneOffset.ToArray();
+    }
+
+
+    void createBones()
+    {
+
+    }
+
+}
